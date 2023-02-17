075e71a3b4d8d942a3d5c4ca8019f673284c163a
Alefe Serafim <correia.alefe@gmail.com>
2023-02-17 07:48:10 -0300

    Resolvendo exercícios do Cap. 02

---
+++ cap02/main.js
@@ -0,0 +1,4 @@
+console.log("Testando JS em arquivo separado.")
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Mudei o texto, otário!";
+document.write("Testando impressão de texto em tela.");
