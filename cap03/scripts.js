3dae6693370ca30e1b549a0d6c9b9f3e16cbad47
Alefe Serafim <correia.alefe@gmail.com>
2023-03-07 21:28:46 -0300

    Solução do Exercício 1 - Primeira abordagem

--- cap03/scripts.js
+++ cap03/scripts.js
@@ -1,6 +1,22 @@
-function invertArray(texto){
-    
-}
+function invertArray(entrada){
+    let inverter = entrada.toString();
+    var invertido = [];
 
-texto = "Teste";
-invertArray(texto)
+    let j = inverter.length -1;
+    let i = 0;
+
+    console.log("length", j);
+    for (i = 0; i<inverter.length; i++){
+        //console.log(entrada[j]);
+        invertido[i] = inverter[j];
+        j--;
+    }
+    //console.log(invertido[0])
+    return invertido.join("");
+}
+//texto = "Teste";
+texto = 123;
+console.log(invertArray(texto));
+//inverso = invertArray(texto);
+//console.log(inverso);
+//document.write(inverso);
