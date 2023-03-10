c3d20783c68bf2cf687f8fa7c62868e1f40e28fb
Alefe Serafim <correia.alefe@gmail.com>
2023-03-09 21:47:56 -0300

    Cap. 3 - POO - Resolvidos os exercícios de 1 a 4

--- cap03/scripts.js
+++ cap03/scripts.js
@@ -1,7 +1,7 @@
+//Exercício 1
 function invertArray(entrada){
     let inverter = entrada.toString();
     var invertido = [];
-
     let j = inverter.length -1;
     let i = 0;
 
@@ -11,12 +11,48 @@ function invertArray(entrada){
         invertido[i] = inverter[j];
         j--;
     }
-    //console.log(invertido[0])
     return invertido.join("");
 }
-//texto = "Teste";
-texto = 123;
-console.log(invertArray(texto));
-//inverso = invertArray(texto);
-//console.log(inverso);
-//document.write(inverso);
+//texto = 123;
+//console.log(invertArray(texto));
+
+
+//Exercício 2
+function combinationArray(texto){
+    var i, acr;
+    var imp = '';
+    for (i=0; i<texto.length; i++){
+        imp=texto[i];
+            console.log(imp);
+                for(j=i+1; j<texto.length; j++){
+                    imp = imp+texto[j];
+                    console.log(imp);
+                }
+            imp = '';
+    }
+
+}
+//combinaArray('doga');
+
+//Exercício 3
+function sortArray(entrada){
+    alphabet = ['A', 'B', 'C', 'D'];
+    entrada_aux = [];
+    var i;
+    for(i=0; i<entrada.length; i++){
+        entrada_aux[i] = entrada[i]; 
+    }
+    console.log(entrada_aux.sort());
+}
+sortArray('Entrada')
+
+//Exercício 4
+function copyArray(entrada){
+    copia = [];
+    var i;
+    for (i=0; i<entrada.length; i++){
+        copia[i] = entrada[i];
+    }
+    return copia;
+}
+
